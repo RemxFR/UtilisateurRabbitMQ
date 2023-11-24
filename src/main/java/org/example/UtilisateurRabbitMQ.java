88f019ace9ddd948dd1d9374e10a8dde0312b78d
RemxFr <epourexisterpro@gmail.com>
2023-11-24 12:25:45 +0100

    création classe utilisateur implémentation des setters et getters et constructeurs sans lombok

--- src/main/java/org/example/UtilisateurRabbitMQ.java
+++ src/main/java/org/example/UtilisateurRabbitMQ.java
@@ -1,12 +1,5 @@
 package org.example;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
-
-
 public class UtilisateurRabbitMQ {
 
     private int id;
