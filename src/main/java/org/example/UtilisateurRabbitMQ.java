43114fb95c58f5697470578f8a2635cbc48e66da
RemxFr <epourexisterpro@gmail.com>
2023-11-24 12:04:18 +0100

    création classe utilisateur

--- src/main/java/org/example/UtilisateurRabbitMQ.java
+++ src/main/java/org/example/UtilisateurRabbitMQ.java
@@ -1,4 +1,26 @@
 package org.example;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
 public class UtilisateurRabbitMQ {
+
+    private int id;
+
+    private String nom;
+
+    private String prenom;
+
+    private String mail;
+
+    private Boolean isEmailConfirmed;
+
 }
